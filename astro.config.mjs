38039a2da5fa4e05675b43a27e0e7c0262cc3864
newdream <newdream0730@gmail.com>
2022-09-16 20:02:59 +0200

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-// import vercel from "@astrojs/vercel/serverless";
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: vercel(),
   output: 'server',
   adapter: netlify(),
   integrations: [react()],
   site: 'https://astro-project.netlify.app',
-  // base: '/gpm-lds-astro',
+  base: '/gpm-lds-astro',
   clientEntrypoint: '@astrojs/react/client.js',
 });
